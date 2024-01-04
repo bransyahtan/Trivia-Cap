fdecebde374575972995128e13d7820e0fd33487
bransyahtan <bransyahhhh@gmail.com>
2024-01-04 11:50:24 +0700

    test

--- FE_React/App.tsx
+++ FE_React/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HAI BRO</Text>
+      <Text>HAI manis</Text>
       <StatusBar style="auto" />
     </View>
   );
