ac1a9d9e8cbdf74b74563908f2d62d192eef4260
bransyahtan <bransyahhhh@gmail.com>
2024-01-04 22:43:10 +0700

    login oauth

--- FE_React/src/screen/LoginScreen.tsx
+++ FE_React/src/screen/LoginScreen.tsx
@@ -6,11 +6,91 @@ import {
   StyleSheet,
   Text,
   View,
-} from "react-native"
-import React from "react"
-import MyButton from "../components/Button"
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import MyButton from "../components/Button";
+import * as Google from "expo-auth-session/providers/google";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import * as WebBrowser from "expo-web-browser";
+
+interface UserInfo {
+  picture?: string;
+  email: string;
+  verified_email: boolean;
+  name: string;
+}
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
+  const [authInProgress, setAuthInProgress] = useState(false);
+  const navigate = useNavigation();
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    webClientId:
+      "226355358927-33ikf3sl0pbvc283t9vk7ehmcrb26nda.apps.googleusercontent.com",
+  });
+
+  // useEffect(() => {
+  //   if (
+  //     authInProgress &&
+  //     response?.type === "success" &&
+  //     response.authentication
+  //   ) {
+  //     getUserInfo(response.authentication.accessToken);
+  //   }
+  // }, [response]);
+
+  const handleEffect = async () => {
+    const user = await getLocalUser();
+
+    if (!user) {
+      setAuthInProgress(true);
+      const result = await promptAsync();
+      console.log(result);
+      if (result.type == "success") {
+        getUserInfo(result?.authentication?.accessToken || "");
+        navigate.navigate("MainApp" as never);
+      }
+    } else {
+      // navigate.navigate("Home" as never);
+      console.log(user);
+      console.log("loaded locally");
+    }
+  };
+
+  const getLocalUser = async () => {
+    try {
+      const data = await AsyncStorage.getItem("user");
+      if (!data) return null;
+      return JSON.parse(data) as UserInfo;
+    } catch (error) {
+      console.log("Error getting local user:", error);
+      return null;
+    }
+  };
+
+  const getUserInfo = async (token: string) => {
+    if (!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+
+      const user = await response.json();
+
+      await AsyncStorage.setItem("user", JSON.stringify(user));
+      setAuthInProgress(false);
+    } catch (error) {
+      console.log("Error fetching user info:", error);
+      setAuthInProgress(false);
+    }
+  };
+
   return (
     <ImageBackground
       source={require("../../assets/images/bg.png")}
@@ -42,12 +122,18 @@ export default function LoginScreen() {
           </View>
 
           <View style={{ marginTop: 10, alignItems: "center" }}>
-            <MyButton text="Login" background="#39A7FF" textColor="white" />
+            <MyButton
+              text="Login"
+              background="#39A7FF"
+              textColor="white"
+              onPress={handleEffect}
+              // navigateTo="MainApp"
+            />
           </View>
         </View>
       </ScrollView>
     </ImageBackground>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
