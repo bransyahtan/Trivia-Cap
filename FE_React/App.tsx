e62b07b1e5b3e89076fb26d074be84e03fffd9f6
Tajul <subqitajul7@gmail.com>
2024-01-04 19:36:06 +0700

    resolve merge

--- FE_React/App.tsx
+++ FE_React/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { NavigationContainer } from "@react-navigation/native"
 import Route from "./src/routes/route"
 
@@ -9,25 +8,3 @@ export default function App() {
     </NavigationContainer>
   )
 }
-=======
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>HAI manis</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
->>>>>>> e02147758b59a6e649aeda9211b09ef287b8e07b
