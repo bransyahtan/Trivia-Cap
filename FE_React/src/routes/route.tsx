83c6be32c6717df0a9bcf517b4eb3f7d83480d4d
luminal0 <luminalwww@gmail.com>
2024-01-05 13:01:15 +0700

    bottom nav updated

--- FE_React/src/routes/route.tsx
+++ FE_React/src/routes/route.tsx
@@ -5,13 +5,31 @@ import HomeScreen from "../screen/HomeScreen";
 import ProfileScreen from "../screen/ProfileScreen";
 import ShopScreen from "../screen/ShopScreen";
 import LoginScreen from "../screen/LoginScreen";
+import { Ionicons } from '@expo/vector-icons'; // Import icons from Expo
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? 'home' : 'home-outline'; // Replace with your home icon names
+          } else if (route.name === "Shop") {
+            iconName = focused ? 'cart' : 'cart-outline'; // Replace with your shop icon names
+          } else if (route.name === "Profile") {
+            iconName = focused ? 'person' : 'person-outline'; // Replace with your profile icon names
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
