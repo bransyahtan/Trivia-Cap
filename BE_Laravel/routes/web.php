65224fb7e9cabdda77aeb8c49d2a8b986d433423
Luthfi <luthfiroma00@gmail.com>
2024-01-05 21:07:06 +0700

    laravel update, fetch data, detail data

--- BE_Laravel/routes/web.php
+++ BE_Laravel/routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
 });
 
+Route::get('/kuis', function () {
+    return view('pages/kuis/kuis');
+});
 
-Route::get('/test', function () {
-    return response()->json([
-        'status' => "OK",
-        "message" => "Learn Laravel"
-    ]);
+Route::get('/kuis/detail/{id}', function () {
+    return view('pages/kuis/detail');
 });
