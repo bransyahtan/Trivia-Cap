c36ae92c6e672f95c71be9b55bf02a4a80c1d16e
Tajul <subqitajul7@gmail.com>
2024-01-12 09:00:48 +0700

    save

--- FE_React/src/components/TopUpButton.tsx
+++ FE_React/src/components/TopUpButton.tsx
@@ -1,47 +1,60 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import React, { useEffect, useState } from "react";
-import { useNavigation } from "@react-navigation/core";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { API } from "../utils/api";
+import {
+  Button,
+  Image,
+  Modal,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import React, { useEffect, useState } from "react"
+import { useNavigation } from "@react-navigation/core"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import { API } from "../utils/api"
+import DiamondItem from "./DiamondItem"
 
 export default function TopUpButton({ onPress }: any) {
-  const [diamond, setDiamond] = useState(0);
-           const [modalVisible, setModalVisible] = useState(false)
+  const [diamond, setDiamond] = useState(0)
+  const [modalVisible, setModalVisible] = useState(false)
   const diamondWallet = async () => {
-    const token = await AsyncStorage.getItem("user");
+    const token = await AsyncStorage.getItem("user")
     const response = await API.get("api/v1/detail-wallet", {
       headers: {
         Authorization: "Bearer " + token,
       },
-    });
-    setDiamond(response.data.data.balance_diamond);
-  };
+    })
+    setDiamond(response.data.data.balance_diamond)
+  }
 
   useEffect(() => {
-    diamondWallet();
-  }, []);
+    diamondWallet()
+  }, [])
 
   return (
-    <View>
-      <TouchableOpacity
-        style={{
-          position: "absolute",
-          top: 20,
-          right: 15,
-          zIndex: 1,
-          backgroundColor: "rgba(0,0,0,0.5)",
-          borderRadius: 10,
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <Image
-          source={require("../../assets/images/diamond.png")}
-          style={{ width: 20, height: 20, marginRight: 5 }}
-        />
-        <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>
-          {diamond}
-        </Text>
+    <>
+      <View>
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            top: 20,
+            right: 15,
+            zIndex: 1,
+            backgroundColor: "rgba(0,0,0,0.5)",
+            borderRadius: 10,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../../assets/images/diamond.png")}
+            style={{ width: 20, height: 20, marginRight: 5 }}
+          />
+          <Text style={{ color: "white", fontSize: 14, fontWeight: "bold" }}>
+            {diamond}
+          </Text>
+        </TouchableOpacity>
+
         <TouchableOpacity
           onPress={() => setModalVisible(true)}
           style={{
@@ -169,4 +182,3 @@ const styles = StyleSheet.create({
     },
   },
 })
-export default ModalEditProfile
