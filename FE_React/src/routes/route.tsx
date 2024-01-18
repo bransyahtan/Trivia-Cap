75c816c4d700fbcc841dd595d28816dfada06cd0
bransyahtan <bransyahhhh@gmail.com>
2024-01-18 09:43:41 +0700

    test

--- FE_React/src/routes/route.tsx
+++ FE_React/src/routes/route.tsx
@@ -34,7 +34,7 @@ const ProfileStackNavigator = () => {
 
 const Route = () => {
   return (
-    <Stack.Navigator initialRouteName="Leaderboard">
+    <Stack.Navigator initialRouteName="Play">
       <Stack.Screen
         name="Login"
         component={LoginScreen}
